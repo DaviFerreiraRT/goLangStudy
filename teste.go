97af43f98297f825b0a879a0f5c5c4502a1b72b1
Davi Ferreira <daviferreirart@gmail.com>
2021-09-19 11:15:57 -0300

    my first golang commit

---
+++ teste.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	adicionar "hello_world/funct"
+)
+
+func Add(value1 int, value2 int) int {
+
+	return value1 + value2
+}
+func Sub(value1 int, value2 int) int {
+	return value1 * value2
+}
+
+func main() {
+	matrix := [6][6]int{
+		{5, 5, 7, 6, 9, 6},
+		{4, 5, 3, 0, 5, 9},
+		{43, 52, 36, 10, 51, 96},
+		{45, 33, 34, 21, 35, 87},
+		{5, 2, 15, 55, 61, 9},
+		{1, 7, 6, 2, 1, 9},
+	}
+	for i := 0; i < len(matrix[i]); i++ {
+		for j := 0; j < len(matrix[j]); j++ {
+			fmt.Printf("Matriz[%d][%d] = %d\n", i, j, matrix[i][j])
+		}
+	}
+	var resultado int = adicionar.Soma(5, 9)
+
+	fmt.Printf("Resultado da soma: %d", resultado)
+
+}
